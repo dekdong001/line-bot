0210e2bee7ec3939cfde66707e628f5ad7573095
dekdong001 <39859968+dekdong001@users.noreply.github.com>
2018-06-02 22:56:02 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['source']['userId'];
-			$text = $text.print_r($event['message']);
+			$text = $text.$event['source']['message'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
